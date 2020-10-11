0cc702b3126dcf4de5adcdbcb0303326ebf63b41
Nasirdin <zhaparkulov02@gmail.com>
2020-10-11 14:20:33 +0600

    new store

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function (){
+$(document).ready(function () {
     $("#slider").owlCarousel({
         items: 1,
         loop: true,
@@ -12,7 +12,7 @@ $(document).ready(function (){
         owl.trigger('prev.owl.carousel', [300]);
     });
 
-    $('ul.catalog__tabs-btns').on('click', 'li:not(.active)', function() {
+    $('ul.catalog__tabs-btns').on('click', 'li:not(.active)', function () {
         $(this)
             .addClass('active').siblings().removeClass('active')
             .closest('div.catalog__tabs').find('div.catalog__content').removeClass('active').eq($(this).index()).addClass('active');
@@ -33,4 +33,41 @@ $(document).ready(function (){
             $('.content-box__list').eq(i).toggleClass('active');
         })
     });
+
+    //modal
+    $('#call').on('click', function () {
+        $('.overlay, .modal-call').fadeIn('slow');
+    });
+
+    $('#consultation').on('click', function () {
+        $('.overlay, .modal').fadeIn('slow');
+    });
+
+    $('.close-modal').on('click', function () {
+        $('.overlay, .modal, .buy-modal').fadeOut('slow');
+    });
+
+    //tel mask
+    $("input[type=tel]").mask("0(999) 99-99-99");
+
+    //scroll up
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 400) {
+            $(".btn-up").fadeIn();
+        } else {
+            $(".btn-up").fadeOut();
+        }
+    });
+
+    //modal for product
+
+    $(".content-box__btn").each(function (i) {
+        $(this).on('click', function () {
+            $('.buy-modal .form__subTitle').text($('.content-box__content .title').eq(i).text());
+            $('.overlay, .buy-modal').fadeIn('slow');
+        })
+    })
+
+
 });
